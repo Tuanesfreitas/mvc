8a3f705004f657c805792712c9af3b06c997d6c4
Tuanesfreitas <tuanesfreitasab@gmail.com>
2023-09-19 20:35:35 -0300

    contollers

--- mvc/src/app.js
+++ mvc/src/app.js
@@ -1,11 +1,13 @@
 import express, { json } from 'express';
 import index from './routes/index.js';
 import db from './database/dbConnect.js';
+import projetos from './models/projetos.js'
 
 const app = express();
 
 app.use(json());
-app.use("/",index);
+app.use("/", index);
+app.use("/doadores", projetos);
 
 db.on("error", console.log.bind(console, 'Erro de conexão'))
 db.once("open", () => {
---
+++ mvc/src/routes/projeto.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import ProjetoController from '../controllers/projetoControllers.js';
+
+const router = express.Router();
+
+router 
+.get("/",ProjetoController.getAllProjetos)
+.post("/",ProjetoController.createProjetos)
+
+export default router;
